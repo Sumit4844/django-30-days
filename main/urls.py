0eca81b13394d203745aec466174c3f2d4e81abf
Sumit Ranjan Jaiswal <sumitjaiswal4844@gmail.com>
2026-01-20 00:25:32 +0530

    Day 4: Add student form

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student_list
+from .views import student_list, student_add
 
 urlpatterns = [
     path('', student_list, name='student_list'),
+    path("add/", student_add, name="student_add"),
 ]
