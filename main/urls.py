8148027589c8fa4dbfa99bb8afa38223f9a29fbe
Sumit Ranjan Jaiswal <sumitjaiswal4844@gmail.com>
2026-01-20 00:32:34 +0530

    Day 5: Edit and delete student

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import student_list, student_add
+from .views import student_list, student_add, student_edit, student_delete
 
 urlpatterns = [
-    path('', student_list, name='student_list'),
+    path("", student_list, name="student_list"),
     path("add/", student_add, name="student_add"),
+    path("edit/<int:pk>/", student_edit, name="student_edit"),
+    path("delete/<int:pk>/", student_delete, name="student_delete"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
 from .forms import StudentForm
 
 def student_list(request):
     students = Student.objects.all()
     return render(request, "student_list.html", {"students": students})
-
 
 def student_add(request):
     if request.method == "POST":
@@ -15,5 +14,26 @@ def student_add(request):
             return redirect("student_list")
     else:
         form = StudentForm()
-
     return render(request, "student_add.html", {"form": form})
+
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("student_list")
+    else:
+        form = StudentForm(instance=student)
+
+    return render(request, "student_edit.html", {"form": form})
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+
+    if request.method == "POST":
+        student.delete()
+        return redirect("student_list")
+
+    return render(request, "student_delete.html", {"student": student})
